1ff0ceb9e1a7ae00e4352ce5f736d949398ad82c
AsadUllahSiddiqui <asadsiddiquidev@gmail.com>
2024-02-21 01:48:03 +0500

    making shop homepage dynamic.

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,7 +2,7 @@
 <html class="no-js" lang="en_AU" />
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo (!empty($title)) ? 'Title-'.$title: 'Home'; ?></title>
+	<title>Onlie Shop</title>
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
 
@@ -31,12 +31,12 @@
 	<meta name="twitter:card" content="summary_large_image" />
 
 
-	<link rel="stylesheet" type="text/css" href="css/slick.css" />
-	<link rel="stylesheet" type="text/css" href="css/slick-theme.css" />
-	<link rel="stylesheet" type="text/css" href="css/video-js.css" />
-    <link rel="stylesheet" type="text/css" href="css/style.css?v=<?php echo rand(111,999); ?>" />
+	<link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/slick.css')}}" />
+	<link rel="stylesheet" type="text/css" href= "{{asset('front-assets/css/slick-theme.css')}}" />
+	{{-- <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/video-js.css')}}"/> --}}
+    <link rel="stylesheet" type="text/css" href="{{asset('front-assets/css/style.css')}}" />
 
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+	<link href="  {{ asset('admin_assets/img/AdminLTELogo.png') }}https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&family=Raleway:ital,wght@0,400;0,600;0,800;1,200&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
@@ -768,12 +768,12 @@
 		</div>
 	</div>
 </footer>
-<script src="js/jquery-3.6.0.min.js"></script>
-<script src="js/bootstrap.bundle.5.1.3.min.js"></script>
-<script src="js/instantpages.5.1.0.min.js"></script>
-<script src="js/lazyload.17.6.0.min.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/custom.js"></script>
+<script src="{{asset('front-assets/js/jquery-3.6.0.min.js')}}"></script>
+<script src="{{asset('front-assets/js/bootstrap.bundle.5.1.3.min.js')}}"></script>
+<script src= "{{asset('front-assets/js/instantpages.5.1.0.min.js')}}"></script>
+<script src="{{asset('front-assets/js/lazyload.17.6.0.min.js')}}"></script>
+<script src="{{asset('front-assets/js/slick.min.js')}}"></script>
+<script src="{{asset('front-assets/js/custom.js')}}"></script>
 <script>
 window.onscroll = function() {myFunction()};
 
